5b74d2111db939c58fd691f71fb82302f27768a6
Hexagon-0 <lucasosouza66@gmail.com>
2024-11-02 00:41:27 -0300

    Add textarea and modifiable canvas

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,36 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function throwExpression (message: string): never {
+  throw new Error(message);
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.addEventListener('DOMContentLoaded', function () {
+  const editor = document.querySelector<HTMLTextAreaElement>('#editor') ?? throwExpression('`#editor` not found');
+  const canvas = document.querySelector<HTMLCanvasElement>('#display') ?? throwExpression('`#display` not found');
+  const ctx = canvas.getContext('2d') ?? throwExpression('Could not obtain 2D context from display');
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const displayData = new Uint8ClampedArray(1024);
+  const displayImage = ctx.createImageData(32, 32);
+
+  const colorMap: Record<number, number> = {
+    [0]: 0x000000ff,
+    [1]: 0xff0000ff,
+    [2]: 0x00ff00ff,
+    [3]: 0x0000ffff,
+    [4]: 0xffffffff,
+  };
+
+  // TEST
+  for (let i = 0; i < displayData.byteLength; i++) {
+    displayData[i] = i % 4 + 1;
+  };
+
+  requestAnimationFrame(function (_time: DOMHighResTimeStamp) {
+    const dv = new DataView(displayImage.data.buffer);
+    for (let i = 0; i < displayData.byteLength; i++) {
+      const index = displayData[i];
+      const value = colorMap[index];
+      dv.setUint32(i * 4, value);
+    }
+
+    ctx.putImageData(displayImage, 0, 0);
+  });
+});
