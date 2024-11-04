1417c6ea5964ae0b24d9b73e99f3541c208a423c
Hexagon-0 <lucasosouza66@gmail.com>
2024-11-04 03:15:28 -0300

    Add "start" and "update" hooks

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import wabt from 'wabt';
 
-
 function throwExpression (message: string): never {
   throw new Error(message);
 }
@@ -8,11 +7,13 @@ function throwExpression (message: string): never {
 document.addEventListener('DOMContentLoaded', function () {
   const editor = document.querySelector<HTMLTextAreaElement>('#editor') ?? throwExpression('`#editor` not found');
   const buttonInstantiate = document.querySelector<HTMLButtonElement>('#instantiate') ?? throwExpression('`#instantiate` not found');
+  const buttonRun = document.querySelector<HTMLButtonElement>('#run') ?? throwExpression('`#run` not found');
+  const buttonStop = document.querySelector<HTMLButtonElement>('#stop') ?? throwExpression('`#stop` not found');
   const canvas = document.querySelector<HTMLCanvasElement>('#display') ?? throwExpression('`#display` not found');
   const ctx = canvas.getContext('2d') ?? throwExpression('Could not obtain 2D context from display');
 
-  const displayMemory = new WebAssembly.Memory({ initial: 1 });
-  const displayData = new Uint8ClampedArray(displayMemory.buffer, 0, 1024);
+  const memory = new WebAssembly.Memory({ initial: 1 });
+  const displayData = new Uint8ClampedArray(memory.buffer, 0x200, 1024);
   const displayImage = ctx.createImageData(32, 32);
 
   const colorMap: Record<number, number> = {
@@ -23,27 +24,64 @@ document.addEventListener('DOMContentLoaded', function () {
     [4]: 0xffffffff,
   };
 
-  requestAnimationFrame(function draw (_time: DOMHighResTimeStamp) {
-    requestAnimationFrame(draw);
-    
+  function draw () {
     const dv = new DataView(displayImage.data.buffer);
     for (let i = 0; i < displayData.byteLength; i++) {
       const index = displayData[i];
-      const value = colorMap[index];
+      const value = colorMap[index] ?? colorMap[0];
       dv.setUint32(i * 4, value);
     }
 
     ctx.putImageData(displayImage, 0, 0);
-  });
+  }
+
+  draw();
+
+  let raf: number;
+  const defaultUpdateFn = function () {};
+  let updateFn: Function = defaultUpdateFn;
+
+  function update (_time: DOMHighResTimeStamp) {
+    raf = requestAnimationFrame(update);
+    updateFn();
+    draw();
+  }
 
   wabt().then(wabt => {
     buttonInstantiate.disabled = false;
+
+    let wasm: WebAssembly.WebAssemblyInstantiatedSource | null;
 
     buttonInstantiate.addEventListener('click', async function (_evt: MouseEvent) {
       const wat = editor.value;
       const module = wabt.parseWat('mylib', wat, { multi_memory: true } as any); // https://github.com/AssemblyScript/wabt.js/pull/50
       const bin = module.toBinary({ log: true });
-      const wasm = await WebAssembly.instantiate(bin.buffer, { js: { display: displayMemory } });
+      wasm = await WebAssembly.instantiate(bin.buffer, { js: { mem: memory } });
+      buttonRun.disabled = false;
+    });
+
+    buttonRun.addEventListener('click', function (_evt) {
+      if (!wasm) return;
+
+      if (typeof wasm.instance.exports.start === 'function') {
+        wasm.instance.exports.start();
+        draw();
+      }
+
+      if (typeof wasm.instance.exports.update === 'function') {
+        updateFn = wasm.instance.exports.update;
+      }
+
+      raf = requestAnimationFrame(update);
+
+      buttonRun.disabled = true;
+      buttonStop.disabled = false;
+    });
+
+    buttonStop.addEventListener('click', function (_evt) {
+      cancelAnimationFrame(raf);
+      buttonRun.disabled = false;
+      buttonStop.disabled = true;
     });
   }).catch(reason => {
     if (typeof reason.message === 'string') {
